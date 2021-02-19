f1212adc9904d6b295470b2950dd37bf36679e2c
Jason Nelson <jasonnelson618@gmail.com>
2021-02-19 09:24:25 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,41 +12,41 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver, 1025))
     recv = clientSocket.recv(1024).decode()
-    print(recv)
-    if recv[:3] != '220':
-        print('220 reply not received from server.')
+    #print(recv)
+    #if recv[:3] != '220':
+        #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
-    if recv1[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv1)
+    #if recv1[:3] != '250':
+        #print('250 reply not received from server.')
 
     # Send MAIL FROM command and print server response.
     mailfrom = 'MAIL FROM: <jen260@nyu.edu>\r\n'
     clientSocket.send(mailfrom.encode())
     recv2 = clientSocket.recv(1024).decode()
-    print(recv2)
-    if recv2[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv2)
+    #if recv2[:3] != '250':
+        #print('250 reply not received from server.')
 
     # Send RCPT TO command and print server response.
     rcptto = 'RCPT TO: <jen260@nyu.edu>\r\n'
     clientSocket.send(rcptto.encode())
     recv3 = clientSocket.recv(1024).decode()
-    print(recv3)
-    if recv3[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv3)
+    #if recv3[:3] != '250':
+        #print('250 reply not received from server.')
 
     # Send DATA command and print server response.
     datatosend = "DATA\r\n"
     clientSocket.send(datatosend.encode())
     recv4 = clientSocket.recv(1024).decode()
-    print(recv4)
-    if recv4[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv4)
+    #if recv4[:3] != '250':
+        #print('250 reply not received from server.')
 
     # Send message data.
     clientSocket.send(msg.encode())
