6f90baf8b10caa1b68184ca4550c98b9b8954207
Jason Nelson <jasonnelson618@gmail.com>
2021-03-18 08:09:35 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -50,7 +50,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         icmpHeader = recPacket[20:28]
         type, code, checksum, packetID, sequence = struct.unpack(
             "bbHHh", icmpHeader
-
+        )
         if type != 8 and packetID == ID:
             bytesInDouble = struct.calcsize("d")
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
